566801ac538fb50bec352c5992a3d76e60e54c56
Yehor <20585619+satelllte@users.noreply.github.com>
2026-09-21 18:10:49 +0300

    unified scene file

--- src/components/Scene.tsx
+++ src/components/Scene.tsx
@@ -1,17 +1,41 @@
+import "./r3f-extend-tsl";
 import { OrbitControls } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
+import { type RendererParameters, WebGPURenderer } from "three/webgpu";
 
-export function Scene() {
+type SceneProps = {
+  engine: "webgl" | "webgpu";
+};
+
+function getGl(engine: SceneProps["engine"]) {
+  if (engine === "webgl") return undefined;
+  return async (props: RendererParameters) => {
+    const renderer = new WebGPURenderer(props);
+    await renderer.init();
+    return renderer;
+  };
+}
+
+export function Scene({ engine }: SceneProps) {
   return (
     <div className="container">
-      <Canvas camera={{ position: [2.5, 2.5, 5.0] }}>
+      <Canvas camera={{ position: [2.5, 2.5, 5.0] }} gl={getGl(engine)}>
         <OrbitControls enableZoom={false} enablePan={false} />
         <ambientLight />
-        <mesh>
-          <boxGeometry />
-          <meshPhysicalMaterial color={0xcc1122} />
-        </mesh>
+        <Box engine={engine} />
       </Canvas>
     </div>
+  );
+}
+
+type BoxProps = Pick<SceneProps, "engine">;
+
+function Box({ engine }: BoxProps) {
+  return (
+    <mesh>
+      <boxGeometry />
+      {engine === "webgl" && <meshPhysicalMaterial color={0xcc1122} />}
+      {engine === "webgpu" && <meshPhysicalNodeMaterial color={0x1122cc} />}
+    </mesh>
   );
 }
