4ae1184c1ff8989c0975bf94ed25e294de78fcca
Yehor <20585619+satelllte@users.noreply.github.com>
2026-09-26 13:50:12 +0300

    docker: sync all playwright artifacts

--- scripts/docker-e2e.sh
+++ scripts/docker-e2e.sh
@@ -5,6 +5,8 @@ docker build -t three-visual-testing-e2e .
 
 docker run --rm --init --ipc=host \
   -v "$(pwd)/tests-e2e:/work/tests-e2e" \
+  -v "$(pwd)/test-results:/work/test-results" \
+  -v "$(pwd)/playwright-report:/work/playwright-report" \
   three-visual-testing-e2e \
   /bin/bash -c '
     pnpm test:e2e "$@"
